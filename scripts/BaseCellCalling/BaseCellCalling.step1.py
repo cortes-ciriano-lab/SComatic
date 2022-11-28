79731a2b34a618dba85abd1d9449c79901babfb3
Francesc Muyas <32641880+Francesc-Muyas@users.noreply.github.com>
2022-11-28 10:40:37 +0000

    Update BaseCellCalling.step1.py

--- scripts/BaseCellCalling/BaseCellCalling.step1.py
+++ scripts/BaseCellCalling/BaseCellCalling.step1.py
@@ -43,11 +43,7 @@ def variant_calling_step1(file,alpha1,beta1,alpha2,beta2,min_ac_cells,min_ac_rea
 					cell_types_idx = {x : elements[x] for x in range(len(elements)) if x > 4} # First 4 columns are common across all lines ['#CHROM', 'Start','End', 'REF', 'INFO']. The rest are cell types
 
 					diff_cell_types = list(cell_types_idx.values())
-
-					# # Let's save the number of informative sites
-					# TEMPLATE = {min_reads:0, min_cells:0, 5: 0, 10: 0, 20: 0,30: 0}
-					# INFORMATIVE_POSITIONS_TEMPLATE = {elements[x]:{'NC':TEMPLATE.copy(),'DP':TEMPLATE.copy()} for x in range(len(elements)) if x > 3}
-
+					
 					# Info header
 					DICT = {'ALT': "##INFO=ALT,Description=Alternative alleles found",
 						'FILTER':"##INFO=FILTER,Description=Filter status of the variant site",
@@ -655,9 +651,9 @@ def build_dict(editing,window):
 def initialize_parser():
 	parser = argparse.ArgumentParser(description='Script to perform the scRNA somatic variant calling')
 	parser.add_argument('--infile', type=str, help='Input file with all samples merged in a single tsv', required = True)   
-	parser.add_argument('--outfile', type=str, help='Out file prefix', required = True)
+	parser.add_argument('--outfile', type=str, help='Output file prefix', required = True)
 	parser.add_argument('--ref', type=str, help='Reference fasta file (*fai must exist)', required = True)
-	parser.add_argument('--editing', type=str, help='Editing file to be used to remove RNA-diting sites', required = False)
+	parser.add_argument('--editing', type=str, help='RNA editing file to be used to remove RNA-diting sites', required = False)
 	parser.add_argument('--pon', type=str, help='Panel of normals (PoN) file to be used to remove germline and false positive calls', required = False)	
 	parser.add_argument('--min_cov', type=int, default = 5, help='Minimum depth of coverage to consider a sample. [Default: 5]', required = False)
 	parser.add_argument('--min_cells', type=int, default = 5, help='Minimum number of cells covering a site to consider a sample. [Default: 5]', required = False)
@@ -667,10 +663,10 @@ def initialize_parser():
 	parser.add_argument('--min_cell_types', type=int, default = 2, help='Minimum number of cell types with enough coverage and cell to consider a site as callable [Default: 2]', required = False)
 	parser.add_argument('--fisher_cutoff', type=float, default = 1, help='P-value cutoff for the Fisher exact test performed to detect strand bias. Expected float value, if applied, we recommend 0.001. By default, this test is switched off with a value of 1 [Default: 1]', required = False)
 	parser.add_argument('--min_distance', type=int, default = 5, help='Minimum distance allowed between potential somatic variants [Default: 5]', required = False)
-	parser.add_argument('--alpha1', type=float, default = 0.260288007167716, help='Alpha parameter for beta distribution of read counts. [Default: 0.260288007167716]', required = False)
-	parser.add_argument('--beta1', type=float, default = 173.94711910763732, help='Beta parameter for beta distribution of read counts. [Default: 173.94711910763732]', required = False)
-	parser.add_argument('--alpha2', type=float, default = 0.08354121346569514, help='Alpha parameter for beta distribution of cell counts. [Default: 0.08354121346569514]', required = False)
-	parser.add_argument('--beta2', type=float, default = 103.47683488327257, help='Beta parameter for beta distribution of cell counts. [Default: 103.47683488327257]', required = False)
+	parser.add_argument('--alpha1', type=float, default = 0.260288007167716, help='Alpha parameter for Beta-binomial distribution of read counts. [Default: 0.260288007167716]', required = False)
+	parser.add_argument('--beta1', type=float, default = 173.94711910763732, help='Beta parameter for Beta-binomial distribution of read counts. [Default: 173.94711910763732]', required = False)
+	parser.add_argument('--alpha2', type=float, default = 0.08354121346569514, help='Alpha parameter for Beta-binomial distribution of cell counts. [Default: 0.08354121346569514]', required = False)
+	parser.add_argument('--beta2', type=float, default = 103.47683488327257, help='Beta parameter for Beta-binomial distribution of cell counts. [Default: 103.47683488327257]', required = False)
 	return (parser)
 
 def main():
@@ -715,36 +711,6 @@ def main():
 	outfile_report_all = outfile + '.coverage_cell_count.report.tsv'
 	outfile_report_all_chrom = outfile + '.coverage_cell_count.per_chromosome.report.tsv'
 	
-	# # Coverages per choromosome
-	# count_df = 0
-	# for chrom in informative_positions.keys():
-	# 	informative_positions_temp = informative_positions[chrom]
-	# 	DF_temp = pd.DataFrame.from_dict({(i,j): informative_positions_temp[i][j] 
-	# 							for i in informative_positions_temp.keys() 
-	# 							for j in informative_positions_temp[i].keys()},
-	# 							orient='index').reset_index()
-	# 	DF_temp = DF_temp.rename(columns={'level_0': 'Cell_type', 'level_1' : 'Count_category'})
-	# 	DF_temp.insert(0, '#CHROM', chrom)
-	# 	if (count_df == 0):
-	# 		DF = DF_temp
-	# 		count_df = count_df + 1
-	# 	else:
-	# 		DF = DF.append(DF_temp, ignore_index=True)
-
-	# DF.to_csv(outfile_report_all_chrom, index=False, sep = '\t')
-
-	# # Total coverage per cell type
-	# f = {x:'sum' for x in DF.columns if not x in ['#CHROM','Cell_type','Count_category']}
-	# DFtotal = DF.groupby(['Cell_type','Count_category']).agg(f).reset_index()
-	# DFtotal.to_csv(outfile_report_all, index=False, sep = '\t')
-
-	# # 1.2: Step 2: Add distance, editing and PoN filters
-	# print ('\n- Variant calling step 2\n')
-	# print("	> Editing file used: " , editing)
-	# print("	> PoN file used: " , pon)
-	# outfile2 = outfile + '.somatic.calling.tsv'
-	# variant_calling_step2(outfile1,distance,editing,pon,dict_variants,window,outfile2)
-	# os.remove(outfile1)
 
 #-------------------------
 # Running scRNA somatic variant calling
